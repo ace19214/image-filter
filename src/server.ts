727a0c047945cd2966b1658eb0ce39823bb71793
ace19214 <duyddse62881@gmail.com>
2022-10-16 14:55:34 +0700

    update code

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from "express";
+import bodyParser from "body-parser";
+import {Request, Response} from "express";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -25,41 +23,33 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
+  //   the filtered image file
   app.get("/filteredimage", async (req, res) => {
-    const imageUrl: string = req.query.image_url;
+    let imageUrl: string = req.query.image_url;
 
     if (!imageUrl) {
-      return res.status(400).send("Image url is required.");
+      return res.status(400).send("Image url is required!");
     }
 
     filterImageFromURL(imageUrl)
       .then((imageFile) =>
         res.status(200).sendFile(imageFile, () => {
           deleteLocalFiles([imageFile]);
+          console.log("Local file deleted.");
         })
-      ).catch((error) => {res
+      )
+      .catch((error) => {
+        console.error("Error occurr at fileteredImage: " + error);
+        res
           .status(500)
           .send(
-            "Unexpected error occurr " + error.message
+            "Unexpected error occurr when filtering image! " + error.message
           );
       });
   });
-
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
-
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
